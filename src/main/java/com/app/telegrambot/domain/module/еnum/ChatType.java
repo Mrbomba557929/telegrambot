c93877db4b01bab39d01cdfb75911cc765da41d7
artyom <sven557929@mail.ru>
2022-02-07 15:55:23 +0300

    Init v2

--- src/main/java/com/app/telegrambot/domain/module/еnum/ChatType.java
+++ src/main/java/com/app/telegrambot/domain/module/еnum/ChatType.java
@@ -1,8 +1,10 @@
 package com.app.telegrambot.domain.module.еnum;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public enum ChatType {
-    PRIVATE,
-    GROUP,
-    SUPERGROUP,
-    CHANNEL
+    @JsonProperty("private") PRIVATE,
+    @JsonProperty("group") GROUP,
+    @JsonProperty("supergroup") SUPERGROUP,
+    @JsonProperty("channel") CHANNEL
 }
