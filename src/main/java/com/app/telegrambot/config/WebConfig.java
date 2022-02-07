41d93fbcf91a3c94d8c61f8e67da7eb06a10b212
artyom <sven557929@mail.ru>
2022-02-07 15:43:31 +0300

    test

--- src/main/java/com/app/telegrambot/config/WebConfig.java
+++ src/main/java/com/app/telegrambot/config/WebConfig.java
@@ -1,9 +1,8 @@
 package com.app.telegrambot.config;
 
+import com.app.telegrambot.factory.StringToEnumConverterFactory;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.convert.converter.Converter;
-import org.springframework.core.convert.converter.ConverterFactory;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -13,20 +12,6 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addFormatters(FormatterRegistry registry) {
-        registry.addConverterFactory(new ConverterFactory<String, Enum>() {
-
-            @SuppressWarnings("unchecked")
-            @Override
-            public <T extends Enum> Converter<String, T> getConverter(Class<T> targetType) {
-                return source -> {
-                    log.error("Я тут");
-                    try {
-                        return (T) Enum.valueOf(targetType, source.toUpperCase());
-                    } catch (Exception e) {
-                        return targetType.getEnumConstants()[Integer.parseInt(source.toUpperCase())];
-                    }
-                };
-            }
-        });
+        registry.addConverterFactory(new StringToEnumConverterFactory());
     }
 }
