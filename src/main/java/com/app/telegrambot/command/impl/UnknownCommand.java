95abde067c3b5896240b32533693da14e44e781c
artyom <sven557929@mail.ru>
2022-02-12 12:03:25 +0300

    edited unknown command

--- src/main/java/com/app/telegrambot/command/impl/UnknownCommand.java
+++ src/main/java/com/app/telegrambot/command/impl/UnknownCommand.java
@@ -3,7 +3,7 @@ package com.app.telegrambot.command.impl;
 import com.app.telegrambot.command.Command;
 import com.app.telegrambot.domain.base.response.Update;
 
-public class UnknownCommand extends Command {
+public class UnknownCommand implements Command {
 
     @Override
     public void execute(Update update) {
