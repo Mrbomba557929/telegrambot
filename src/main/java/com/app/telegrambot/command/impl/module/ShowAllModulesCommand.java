dc6bfdb08c4e386527fbc41b04e97d342d307546
artyom <sven557929@mail.ru>
2022-02-13 20:24:17 +0300

    test

--- src/main/java/com/app/telegrambot/command/impl/module/ShowAllModulesCommand.java
+++ src/main/java/com/app/telegrambot/command/impl/module/ShowAllModulesCommand.java
@@ -80,7 +80,7 @@ public class ShowAllModulesCommand implements Command {
                     .replyMarkup(inlineKeyboardPaginator.paginate(
                             modules.getNumber(), modules.getTotalPages(), TOTAL_PAGES_IN_BLOCK,
                             numberOfBlock, firstPage, lastPage))
-                    .parseMode(ParseMode.HTML)
+                    .parseMode(ParseMode.MARKDOWN)
                     .build());
 
         } catch (TelegramApiException e) {
